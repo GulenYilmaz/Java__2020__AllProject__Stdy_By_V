ce7cdc57e66a990ce054fc9c4da9764232c5eb60
GulenYilmaz <gulenaltintasyilmaz@gmail.com>
2020-05-13 21:42:43 -0400

    new

---
+++ src/F__Dibi__Selenyum/VideosWEBaddress.java
@@ -0,0 +1,15 @@
+package F__Dibi__Selenyum;
+/*
+https://www.youtube.com/watch?v=P-UXEpMSH18&feature=youtu.be Selenium 1
+https://www.youtube.com/watch?v=yL8c15-iGv4&feature=youtu.be Selenium 2
+https://www.youtube.com/watch?v=-LZdiC7c2nQ&feature=youtu.be Selenium 3
+https://www.youtube.com/watch?v=EP4XxmEs2Y4&feature=youtu.be Selenium 4
+https://www.youtube.com/watch?v=RbK94W1plUQ&feature=youtu.be Selenium 5
+https://www.youtube.com/watch?v=7-2gnkrNmTY&feature=youtu.be Selenium 6
+https://www.youtube.com/watch?v=5Cxw2tFo26o&feature=youtu.be Selenium 7
+ * 
+ * 
+ */
+public class VideosWEBaddress {
+
+}
